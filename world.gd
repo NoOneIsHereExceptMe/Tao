308958278acf531bd292d01de134d9fa08152477
JackOriander <aritramalik0@gmail.com>
2024-04-30 20:16:57 +0530

    Update Main.png, Main.png.import, YinYang.png, and 5 more files

---
+++ world.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_texture_button_pressed():
+	print("image pressed")
+
+
+func _on_hunt_button_pressed():
+	print("hunt button pressed")
